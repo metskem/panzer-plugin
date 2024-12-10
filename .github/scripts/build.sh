76df0142b6de24c333ba97891807c20583c7641f
Harry Metske <harry.metske@gmail.com>
2024-12-10 16:55:58 +0100

    fix name of plugin from npsb to panzer

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,7 +3,7 @@
 OUTPUT_DIR=$PWD/dist
 mkdir -p "${OUTPUT_DIR}"
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${OUTPUT_DIR}/npsb-plugin_linux_amd64 -ldflags "-X github.com/metskem/panzer-plugin/version.VERSION=${VERSION} -X github.com/metskem/panzer-plugin/version.COMMIT=${COMMIT}" .
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ${OUTPUT_DIR}/npsb-plugin_darwin_amd64 -ldflags "-X github.com/metskem/panzer-plugin/version.VERSION=${VERSION} -X github.com/metskem/panzer-plugin/version.COMMIT=${COMMIT}" .
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ${OUTPUT_DIR}/npsb-plugin_darwin_arm64 -ldflags "-X github.com/metskem/panzer-plugin/version.VERSION=${VERSION} -X github.com/metskem/panzer-plugin/version.COMMIT=${COMMIT}" .
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ${OUTPUT_DIR}/npsb-plugin_windows_amd64 -ldflags "-X github.com/metskem/panzer-plugin/version.VERSION=${VERSION} -X github.com/metskem/panzer-plugin/version.COMMIT=${COMMIT}" .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${OUTPUT_DIR}/panzer-plugin_linux_amd64 -ldflags "-X github.com/metskem/panzer-plugin/version.VERSION=${VERSION} -X github.com/metskem/panzer-plugin/version.COMMIT=${COMMIT}" .
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ${OUTPUT_DIR}/panzer-plugin_darwin_amd64 -ldflags "-X github.com/metskem/panzer-plugin/version.VERSION=${VERSION} -X github.com/metskem/panzer-plugin/version.COMMIT=${COMMIT}" .
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ${OUTPUT_DIR}/panzer-plugin_darwin_arm64 -ldflags "-X github.com/metskem/panzer-plugin/version.VERSION=${VERSION} -X github.com/metskem/panzer-plugin/version.COMMIT=${COMMIT}" .
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ${OUTPUT_DIR}/panzer-plugin_windows_amd64 -ldflags "-X github.com/metskem/panzer-plugin/version.VERSION=${VERSION} -X github.com/metskem/panzer-plugin/version.COMMIT=${COMMIT}" .
